7e6a3c94a12110a958bd773de8b1c363a2ca3926
juangro <ing.juangro@gmail.com>
2019-02-12 10:51:15 -0600

    Add basics

---
+++ intro/intro/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  intro
+//
+//  Created by Juan Hernández on 2/12/19.
+//  Copyright © 2019 Juan Hernández. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
